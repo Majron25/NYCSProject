d8745ac5b35209a71ce07e3daaee44820e510ea9
Majron25 <filip.tlatlik1@gmail.com>
2024-10-23 15:40:47 +0200

    Start of the Login function

---
+++ my-app/src/pages/account.js
@@ -0,0 +1,17 @@
+
+
+
+export default function account(){
+
+
+    return(
+        <>
+        <div>
+            hello there
+            <div>
+                Co tutaj można dodać ?  
+            </div>
+        </div>
+        </>
+    );
+}
